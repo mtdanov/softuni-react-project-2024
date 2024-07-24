00656a540859f9c3dd8f53a36ee7eee7c410e48b
mtdanov <mtdanov@gmail.com>
2024-07-24 17:26:29 +0300

    login and logout service

--- client/src/services/authService.jsx
+++ client/src/services/authService.jsx
@@ -3,20 +3,32 @@ import * as request from "../lib/request";
 let baseUrl = "http://localhost:3010";
 
 export const register = (name, lastName, email, password, rePass, country, city, street, streetNumber) => {
-  try {
-    const result = request.post(`${baseUrl}/register`, {
-      name,
-      lastName,
-      email,
-      password,
-      rePass,
-      country,
-      city,
-      street,
-      streetNumber
-    });
-    return result;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const result = request.post(`${baseUrl}/register`, {
+            name,
+            lastName,
+            email,
+            password,
+            rePass,
+            country,
+            city,
+            street,
+            streetNumber
+        });
+        return result;
+    } catch (error) {
+        throw error;
+    }
 };
+
+export const login = (email, password) => {
+    try {
+        const result = request.post(`${baseUrl}/login`, { email, password });
+        return result;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const logout = (accessToken) =>
+    request.post(`${baseUrl}/logout`, undefined, accessToken);
